77387de586e457e217b74e941abaaf5935a58f49
Bohdan <bohdan.kornatskyi@gmail.com>
2021-12-20 19:50:33 -0800

    added tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState(["Learn React", "Learn Vue"]);
+
+  const onSummit = (e) => {
+    e.preventDefault();
+
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todo app </h1>
       </header>
+      <main>
+        <form>
+          <input type="text" name="task" placeholder="Hello" onChange={(e) => {
+            setTask(e.target.value);
+          }} />
+          <button onClick={onSummit}>Add</button>
+        </form>
+
+        <ul>
+          {
+            tasks.map((task, index) => {
+              return <li key={index}>{task}</li>
+            }
+            )
+          }
+        </ul>
+
+      </main>
     </div>
   );
 }
