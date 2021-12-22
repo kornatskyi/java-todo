867fdba977358084a52e9cd8cae48086f5e55dee
Bohdan <bohdan.kornatskyi@gmail.com>
2021-12-21 20:21:32 -0800

    hot reload spring

--- src/main/java/com/example/ToDoController.java
+++ src/main/java/com/example/ToDoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloWorld {
+public class ToDoController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -16,6 +16,7 @@ public class HelloWorld {
     @GetMapping("/hello-world")
     @ResponseBody
     public Greeting sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
